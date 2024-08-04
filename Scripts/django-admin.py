fa1f9060bdb7fdc8506664c7c6606e68e7082da1
Nglassburn <ndglassburn@gmail.com>
2024-08-04 14:37:56 -0400

    My previous work

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\nglas\OneDrive\Documents\CTU\CS492\LavenderBookstore\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ books/urls.py
@@ -0,0 +1,26 @@
+"""ManagementSystem URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .views import BooksListView, BooksDetailView, BookCheckoutView, paymentComplete, SearchResultsListView
+
+
+urlpatterns = [
+    path('', BooksListView.as_view(), name = 'list'),  # the home page or list page
+    path('<int:pk>/', BooksDetailView.as_view(), name = 'detail'),
+    path('<int:pk>/checkout/', BookCheckoutView.as_view(), name = 'checkout'),
+    path('complete/', paymentComplete, name = 'complete'),
+    path('search/', SearchResultsListView.as_view(), name = 'search_results'),
+] 
